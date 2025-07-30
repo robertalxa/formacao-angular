d3bb7688b9f6e090996598cd5167790b526357c9
Robert Alexandre <robert.alexandrea7@gmail.com>
2025-07-29 22:50:11 -0300

    Trabalhando com paginação na listagem dos pensamentos

--- 2438-angular-memoteca/src/app/componentes/pensamentos/pensamento.service.ts
+++ 2438-angular-memoteca/src/app/componentes/pensamentos/pensamento.service.ts
@@ -1,6 +1,6 @@
 // ng g s componentes/pensamentos/pensamento esse é o comando para gerar um service
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Pensamento } from './pensamento';
 import { Observable } from 'rxjs';
 
@@ -11,8 +11,18 @@ export class PensamentoService {
   private readonly API = 'http://localhost:3000/pensamentos';
   constructor(private http: HttpClient) {}
 
-  listar(): Observable<Pensamento[]> {
-    return this.http.get<Pensamento[]>(this.API);
+  listar(pagina: number): Observable<Pensamento[]> {
+    const itensPorPagina = 6;
+
+    let params = new HttpParams()
+      .set('_page', pagina)
+      .set('_limit', itensPorPagina);
+
+    return this.http.get<Pensamento[]>(this.API, { params });
+
+    // return this.http.get<Pensamento[]>(
+    //   `${this.API}?_page=${pagina}&_limit=${itensPorPagina}`
+    // );
   }
 
   criar(pensamento: Pensamento): Observable<Pensamento> {
