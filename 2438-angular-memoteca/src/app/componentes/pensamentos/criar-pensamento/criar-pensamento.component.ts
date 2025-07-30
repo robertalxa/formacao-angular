ca7d93d826c1dc39bc7f4a0ce2fd037f4725faaa
Robert Alexandre <robert.alexandrea7@gmail.com>
2025-07-29 22:15:05 -0300

    Aplicado validator de letras minúsculas

--- 2438-angular-memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ 2438-angular-memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -3,6 +3,7 @@ import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { minusculoValidator } from './minusculoValidators';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -34,6 +35,7 @@ export class CriarPensamentoComponent implements OnInit {
           Validators.required,
           Validators.pattern(regexEmptyValue),
           Validators.minLength(3),
+          minusculoValidator,
         ]),
       ],
       modelo: ['modelo1'],
