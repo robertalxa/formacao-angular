8ebfd9471a461b88e7f0f040f89bde85f1a2cd46
Robert Alexandre <robert.alexandrea7@gmail.com>
2025-10-23 19:55:21 -0300

    Melhorada busca e listagem de favoritos

--- 2438-angular-memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ 2438-angular-memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -12,12 +12,13 @@ export class ListarPensamentoComponent implements OnInit {
   paginaAtual: number = 1;
   haMaisPensamentos: boolean = true;
   filtro: string = '';
+  favoritos: boolean = false;
 
   constructor(private service: PensamentoService) {}
 
   ngOnInit(): void {
     this.service
-      .listar(this.paginaAtual, this.filtro)
+      .listar(this.paginaAtual, this.filtro, this.favoritos)
       .subscribe((listaPensamentos) => {
         this.listaPensamentos = listaPensamentos;
       });
@@ -25,7 +26,7 @@ export class ListarPensamentoComponent implements OnInit {
 
   carregarMaisPensamentos() {
     this.service
-      .listar(++this.paginaAtual, this.filtro)
+      .listar(++this.paginaAtual, this.filtro, this.favoritos)
       .subscribe((listaPensamentos) => {
         this.listaPensamentos.push(...listaPensamentos);
         if (!listaPensamentos.length) {
@@ -38,17 +39,18 @@ export class ListarPensamentoComponent implements OnInit {
     this.haMaisPensamentos = true;
     this.paginaAtual = 1;
     this.service
-      .listar(this.paginaAtual, this.filtro)
+      .listar(this.paginaAtual, this.filtro, this.favoritos)
       .subscribe((pensamentos) => {
         this.listaPensamentos = pensamentos;
       });
   }
 
   listarFavoritos() {
+    this.favoritos = true;
     this.haMaisPensamentos = true;
     this.paginaAtual = 1;
     this.service
-      .listarPensamentosFavoritos(this.paginaAtual, this.filtro)
+      .listar(this.paginaAtual, this.filtro, this.favoritos)
       .subscribe((listaFavoritos) => {
         this.listaPensamentos = listaFavoritos;
       });
--- 2438-angular-memoteca/src/app/componentes/pensamentos/pensamento.service.ts
+++ 2438-angular-memoteca/src/app/componentes/pensamentos/pensamento.service.ts
@@ -11,7 +11,11 @@ export class PensamentoService {
   private readonly API = 'http://localhost:3000/pensamentos';
   constructor(private http: HttpClient) {}
 
-  listar(pagina: number, filtro: string): Observable<Pensamento[]> {
+  listar(
+    pagina: number,
+    filtro: string,
+    favoritos: boolean
+  ): Observable<Pensamento[]> {
     const itensPorPagina = 6;
 
     let params = new HttpParams()
@@ -22,29 +26,15 @@ export class PensamentoService {
       params = params.set('q', filtro.trim());
     }
 
+    if (favoritos) {
+      params = params.set('favorito', true);
+    }
+
     return this.http.get<Pensamento[]>(this.API, { params });
 
     // return this.http.get<Pensamento[]>(
     //   `${this.API}?_page=${pagina}&_limit=${itensPorPagina}`
     // );
-  }
-
-  listarPensamentosFavoritos(
-    pagina: number,
-    filtro: string
-  ): Observable<Pensamento[]> {
-    const itensPorPagina = 6;
-
-    let params = new HttpParams()
-      .set('_page', pagina)
-      .set('_limit', itensPorPagina)
-      .set('favorito', true);
-
-    if (filtro.trim().length > 2) {
-      params = params.set('q', filtro.trim());
-    }
-
-    return this.http.get<Pensamento[]>(this.API, { params });
   }
 
   criar(pensamento: Pensamento): Observable<Pensamento> {
